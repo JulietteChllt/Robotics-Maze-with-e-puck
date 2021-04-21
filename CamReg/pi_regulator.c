80652863999af302e75f97d659608ddb9d252ef6
balt-git <balthazar.bujard@epfl.ch>
2021-04-21 22:38:24 +0200

    regulateur pi fonctionne pas trop mal

--- CamReg/pi_regulator.c
+++ CamReg/pi_regulator.c
@@ -13,7 +13,7 @@
 #include <process_image.h>
 #include <controle.h>
 
-#define WORKING_SPEED 200
+#define WORKING_SPEED 300
 
 static THD_WORKING_AREA(waPiRegulator, 256);
 static THD_FUNCTION(PiRegulator, arg) {
@@ -24,7 +24,7 @@ static THD_FUNCTION(PiRegulator, arg) {
     systime_t time;
 
     int16_t err=0, sum_err=0;
-    uint8_t kp=20, ki=2;
+    uint8_t kp=10, ki=3, i=0;
     int16_t command=0;
     while(1){
         time = chVTGetSystemTime();
@@ -33,7 +33,7 @@ static THD_FUNCTION(PiRegulator, arg) {
 		*	To complete
 		*/
         err = (int16_t) get_calibrated_prox(SENSORRIGHT) - get_reference();
-        command = abs(kp*err); //ajouter ki
+        command = abs(kp*err + ki*sum_err); //ajouter ki
 
 
 
@@ -46,13 +46,16 @@ static THD_FUNCTION(PiRegulator, arg) {
         	left_motor_set_speed(WORKING_SPEED);
         }
 
-        chprintf((BaseSequentialStream *) &SDU1, "valeur senseur droit = %d, ref= %d err= %d\n", get_calibrated_prox(SENSORRIGHT),get_reference(), err);
+        if(i==10)
+        	//chprintf((BaseSequentialStream *) &SDU1, "valeur senseur droit = %d, ref= %d err= %d. sum_err = %d, commande = %d\n", get_calibrated_prox(SENSORRIGHT),get_reference(), err, sum_err, command);
 
-        /*sum_err += err;
-        if(sum_err > MOTOR_SPEED_LIMIT/ki)
-        	sum_err = MOTOR_SPEED_LIMIT/ki;*/
+        sum_err += err;
+        if(sum_err > (WORKING_SPEED)/ki)
+        	sum_err = WORKING_SPEED/ki;
+        else if(sum_err< -(WORKING_SPEED)/ki)
+        	sum_err = -WORKING_SPEED/ki;
 
-
+        i++;
         //applies the speed from the PI regulator
 
 
